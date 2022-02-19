d615a57ff5ca59b7e0b31304e1246dc0839e2549
macielcalebe <macielcalebe@gmail.com>
2022-02-19 12:45:20 -0300

    update repository

--- 02-funcoes/solucao_gabarito.c
+++
@@ -1,6 +0,0 @@
-// TODO implemente uma funcao chamada solucao aqui
-
-int solucao(int a, int b, int *c) {
-    *c = 2*a + 3*b;
-    return 2*a + 5*b;
-}
--- 05-variaveis-locais/solucao_ex4_gabarito.c
+++
@@ -1,8 +0,0 @@
-int ex4_solucao(int d) {
-    int n, i ;
-    scanf("%d %d", &n, &i);
-    i = i * 2;
-    n = n + i;
-    
-    return d + n;
-}
